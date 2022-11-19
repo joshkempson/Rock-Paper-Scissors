29f2818caaa4a1ee5bbd7bb623d2babc8125a96a
Josh <joshuakempson99@gmail.com>
2022-11-19 21:32:48 +0000

    editted js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-if (
-    
-)
+const userInput = prompt("Rock, Paper or Scissors?")
+const computerChoice = ""
+
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice == computerChoice) {
+        console.log("Draw!")
+    } else {
+        console.log("A")
+    }
+}
+
+function getComputerChoice() {
+    var computerInput = Math.floor(Math.random() * 9)
+
+    if (computerInput > 3) {
+        computerChoice = "rock"
+    } else if (computerInput < 5) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    }
+
+    return computerChoice
+}
+
+playRound(userInput, computerChoice)
